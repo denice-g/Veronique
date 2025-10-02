0a1159856742b54328480a9417beeb48ffd35c66
Mooncraft398 <MooncraftGod06@gmail.com>
2025-10-02 17:20:48 -0400

    Added Player Movement, Sprites, and Basic Floor

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private Animator animator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+    if (moveInput > 0)     
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    else if (moveInput < 0)
+    {
+        transform.localScale = new Vector3(-1, 1, 1);
+    }
+        SetAnimation(moveInput);
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void SetAnimation(float moveInput)
+    {
+        if (isGrounded)
+        {
+            if (moveInput == 0)
+            {
+                animator.Play("Player_Idle");
+            }
+
+            else
+            {
+                animator.Play("Player_Walk");
+            }
+        }
+
+        else
+        {
+            if (rb.linearVelocityY > 0)
+            {
+                animator.Play("Player_Jump");
+            }
+
+            else
+            {
+                animator.Play("Player_Fall");
+            }
+        }
+    }
+}
+
