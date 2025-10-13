85c2b25bc2e48b3acc01d4e264c85dd438523278
Kermit0621 <137748396+Kermit0621@users.noreply.github.com>
2025-10-13 15:15:43 -0400

    fixing compilation errors with player.cs

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-<<<<<<< HEAD
-    public float jumpForce = 7f;
-=======
     public float jumpForce = 8f;
->>>>>>> main
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
