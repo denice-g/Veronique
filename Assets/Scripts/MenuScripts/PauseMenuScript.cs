475a33d5cdab31d57a789885d8b4358284e8c417
afedyanin2 <al534167@ucf.edu>
2025-10-27 08:56:40 -0400

    Added music, ability to change audio settings, made pausemenu and audiomanager indestructable prefab objects

--- Assets/Scripts/MenuScripts/PauseMenuScript.cs
+++ Assets/Scripts/MenuScripts/PauseMenuScript.cs
@@ -1,16 +1,78 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PauseScript : MonoBehaviour
 {
     public static bool GameisPaused = false;
 
-    public GameObject pauseMenuUI;
+    [Header("---------- MenuUIs ----------")]
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject optionsMenuUI;
+    [SerializeField] private GameObject audioMenuUI;
+    [SerializeField] private GameObject confirmMenuUI;
+
+    [Header("---------- Buttons ----------")]
+    [SerializeField] private GameObject confirmQuitButton;
+    [SerializeField] private GameObject confirmExitButton;
+
+    private string currentScene;
+    private static PauseScript instance;
+
+    private void Awake()
+    {
+        // Destroy duplicates
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        // Subscribe to sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        // Initialize currentScene in case this object starts in a scene
+        currentScene = SceneManager.GetActiveScene().name;
+        CheckScene();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.name;
+        CheckScene();
+    }
+
+    private void CheckScene()
+    {
+        if (currentScene == "MainMenu")
+        {
+            // Close all menus
+            pauseMenuUI.SetActive(false);
+            optionsMenuUI.SetActive(false);
+            audioMenuUI.SetActive(false);
+            confirmMenuUI.SetActive(false);
+            confirmQuitButton.SetActive(false);
+            confirmExitButton.SetActive(false);
+
+            GameisPaused = false;
+            Time.timeScale = 1f; // Ensure time is running
+        }
+    }
 
     void Update()
     {
         //Pause/Resume game everytime escape is clicked
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && currentScene != "MainMenu")
         {
             if(GameisPaused)
             {
@@ -25,7 +87,17 @@ public class PauseScript : MonoBehaviour
 
     public void Resume()
     {
+        //Close all menus
         pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
+        audioMenuUI.SetActive(false);
+        confirmMenuUI.SetActive(false);
+        confirmQuitButton.SetActive(false);
+        confirmExitButton.SetActive(false);
+
+        //Clear last used button to prevent continued button highlighting
+        EventSystem.current.SetSelectedGameObject(null);
+
         Time.timeScale = 1f;
         GameisPaused = false;
     }
@@ -36,17 +108,72 @@ public class PauseScript : MonoBehaviour
         Time.timeScale = 0f;
         GameisPaused = true;
     }
-
-    //When quit button clicked
-    public void quitToMenu()
+    
+    //When options button is clicked
+    public void Options()
     {
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    //When audio button is clicked (in options menu)
+    public void AudioSettings()
+    {
+        optionsMenuUI.SetActive(false);
+        audioMenuUI.SetActive(true);
+    }
+
+    //When back button is clicked (if in audio menu)
+    public void ReturnToPauseMenu()
+    {
+        optionsMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    //When back button is clicked (if in options menu)
+    public void ReturnToOptionsMenu()
+    {
+        audioMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    //When quit button is clicked
+    public void QuitToMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        confirmMenuUI.SetActive(true);
+        confirmQuitButton.SetActive(true);
+    }
+
+    //When exit button is clicked
+    public void ExitGame()
+    {
+        pauseMenuUI.SetActive(false);
+        confirmMenuUI.SetActive(true);
+        confirmExitButton.SetActive(true);
+    }
+
+    //When yes is clicked (for quit to menu)
+    public void ConfirmQuit()
+    {
+        // Close all menus
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
+        audioMenuUI.SetActive(false);
+        confirmMenuUI.SetActive(false);
+        confirmQuitButton.SetActive(false);
+        confirmExitButton.SetActive(false);
+
+
+
         Time.timeScale = 1f;
         GameisPaused = false;
+        //Object.Destroy(gameObject);
         SceneManager.LoadScene("MainMenu");
     }
 
-    //When exit button clicked
-    public void exitGame()
+    //When yes is clicked (for exit game)
+    public void ConfirmExit()
     {
         Application.Quit();
 
@@ -54,5 +181,16 @@ public class PauseScript : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+
+    //If no is clicked
+    public void No()
+    {
+        confirmMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+
+        //Deactivate both confirm buttons
+        confirmQuitButton.SetActive(false);
+        confirmExitButton.SetActive(false);
     }
 }
